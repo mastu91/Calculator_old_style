9bc1581976e27a20e04af473478a5f0cd92f34fe
Unknown <stutz-marco@gmx.ch>
2018-01-29 17:01:16 +0100

    new Project

---
+++ Calculator_old_style/CalculatorService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_old_style
+{
+    class CalculatorService
+    {
+        private string sign;
+        private string valueOne;
+
+        public CalculatorService()
+        {
+
+        }
+
+        public void SetOperator(string output, string sign)
+        {
+            this.sign = sign;
+            valueOne = output;
+        }
+
+        public string Equalisation(string output)
+        {
+            double numOne = Convert.ToDouble(valueOne);
+            double numTwo = Convert.ToDouble(output);
+            switch (sign)
+            {
+                case "+":
+                    output = (numOne + numTwo).ToString();
+                    break;
+                case "-":
+                    output = (numOne - numTwo).ToString();
+                    break;
+                case "*":
+                    output = (numOne * numTwo).ToString();
+                    break;
+                case "/":
+                    output = (numOne / numTwo).ToString();
+                    break;
+            }
+            return output;
+        }
+
+        public string Reset()
+        {
+            sign = null;
+            valueOne = null;
+            return "";
+        }
+
+        public string ChangeSign(string tbNumber)
+        {
+            double num = Convert.ToDouble(tbNumber);
+            if(num < 0)
+            {
+                string[] number = tbNumber.Split('-');
+                tbNumber = number[1];
+            }
+            if(num > 0)
+            {
+                tbNumber = "-" + Convert.ToString(num);
+            }
+            return tbNumber;
+        }
+
+        public string ResetNumber(string number)
+        {
+            return number.Substring(0, number.Length - 1);
+        }
+    }
+}
--- Calculator_old_style/MainWindow.xaml.cs
+++ Calculator_old_style/MainWindow.xaml.cs
@@ -20,9 +20,111 @@ namespace Calculator_old_style
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly CalculatorService calculatorService = new CalculatorService();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text += "0";
+        }
+
+        private void btnOne_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text += "1";
+        }
+
+        private void btnTwo_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text += "2";
+        }
+
+        private void btnThree_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text += "3";
+        }
+
+        private void btnFour_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text += "4";
+        }
+
+        private void btnFive_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text += "5";
+        }
+
+        private void btnSix_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text += "6";
+        }
+
+        private void btnSeven_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text += "7";
+        }
+
+        private void btnEight_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text += "8";
+        }
+
+        private void btnNine_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text += "9";
+
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            calculatorService.SetOperator(tbOutput.Text, "+");
+            tbOutput.Text = "";
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            calculatorService.SetOperator(tbOutput.Text, "-");
+            tbOutput.Text = "";
+        }
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            calculatorService.SetOperator(tbOutput.Text, "*");
+            tbOutput.Text = "";
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            calculatorService.SetOperator(tbOutput.Text, "/");
+            tbOutput.Text = "";
+        }
+
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text = calculatorService.Equalisation(tbOutput.Text);
+        }
+
+        private void btnDeletAll_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text = calculatorService.Reset();
+        }
+
+        private void btnDeletLastValue_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text = "";
+        }
+
+        private void btnDeletLastInput_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text = calculatorService.ResetNumber(tbOutput.Text);
+        }
+
+        private void btnChangeSign_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text = calculatorService.ChangeSign(tbOutput.Text);
         }
     }
 }
